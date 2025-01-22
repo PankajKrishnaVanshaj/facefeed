3c015a223211b7b6b64e22379b891205438eb030
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-23 03:32:37 +0530

    .env rm

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIO(server, {
   cors: {
-    origin: [`${process.env.CLIENT_HOST}`],
+    origin: ["https://facefeed.pankri.com"],
     methods: ["GET", "POST"],
     credentials: true,
   },
